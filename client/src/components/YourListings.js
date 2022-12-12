f45531006ebe978dc3ebb3c10a4a302118c79e63
Danial Toor <dtoor14@gmail.com>
2022-12-12 00:06:19 -0600

    added rows and columns

--- client/src/components/YourListings.js
+++ client/src/components/YourListings.js
@@ -1,10 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function YourListings() {
     return (
-        <div>
-        <p><b>Witcher 3</b></p>
-        <img style={{ width: 200, height: 250 }} src="https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2945734-the%20witcher%203%20-%20wild%20hunt.jpg" alt="React Image" />
-        <p><b>Price: $20</b></p>
-        </div>
+        <Container>
+          <Row>
+            <Col>Witcher 3</Col>
+            <Col><p className="text-center" >Witcher 3</p></Col>
+          </Row>
+          <Row>
+            <Col  xs={12} sm={20} md={5}><img style={{ width: 200, height: 250 }} src="https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2945734-the%20witcher%203%20-%20wild%20hunt.jpg" alt="React Image" /> </Col>
+            <Col><img style={{ width: 200, height: 250 }} src="https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2945734-the%20witcher%203%20-%20wild%20hunt.jpg" alt="React Image" /> </Col>
+          </Row>
+            <br></br>
+            <br></br>
+          <Row>
+            <Col><img style={{ width: 200, height: 250 }} src="https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2945734-the%20witcher%203%20-%20wild%20hunt.jpg" alt="React Image" /> </Col>
+            <Col><img style={{ width: 200, height: 250 }} src="https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2945734-the%20witcher%203%20-%20wild%20hunt.jpg" alt="React Image" /> </Col>
+          </Row>
+        </Container>
     );
   }
 
