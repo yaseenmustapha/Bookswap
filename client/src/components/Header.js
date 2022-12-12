e9fbc25d7ea64393725fe6bd04f64dfd38b14206
Yaseen Mustapha <ymustapha@deltacontrols.com>
2022-12-11 22:11:17 -0800

    Added icons to search bar and header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+
+const notificationsIcon = (<FontAwesomeIcon icon={faBell} />)
+const username = "sexybeast123"
 
 function Header() {
     return (
@@ -12,8 +17,8 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <NavDropdown title="Notifications" id="basic-nav-dropdown">
+              <Nav.Link href="#home">Welcome, {username}</Nav.Link>
+              <NavDropdown title={notificationsIcon} id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Notification 1</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Notification 2</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Notification 3</NavDropdown.Item>
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function SearchBar() {
     return (
         <InputGroup size="lg">
+            <InputGroup.Text style={{color: "#676869", border:"none", backgroundColor:"white"}}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </InputGroup.Text>
             <Form.Control
             aria-label="Large"
             aria-describedby="inputGroup-sizing-sm"
             className="rounded-pill"
+            placeholder="Search for a game..."
             />
         </InputGroup>
 
