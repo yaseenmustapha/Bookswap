1bc3eeee79de808d4d56603079a9ef8b91be52b9
Yaseen Mustapha <ymustapha@deltacontrols.com>
2022-12-12 19:55:44 -0800

    Use pymongo to hook flask to mongodb for listings

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ source venv/bin/activate
 
 Once you're in virtual env,
 ```
-pip3 install Flask
+pip3 install Flask pymongo
 python3 server.py
 ```
 Try http://localhost:5000/members or http://127.0.0.1:5000/members in your browser to test API call.
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,23 +1,28 @@
+import json
+from bson import json_util
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+ip = 'localhost'
+client = MongoClient(ip, 27017)
+db = client.gameswap_db
+listings_collection = db.listings_collection
+listings_collection.find_one()
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
 # Members API Route
-
-
 @app.route("/members")
 def members():
     return {"members": ["Danial", "Rather", "YJ"]}
 
+# Listings API Route
 @app.route("/listings")
 def listing():
-    return {"listings": [
-            {"name": "The Witcher 3: Wild Hunt", "img": "https://images.igdb.com/igdb/image/upload/t_cover_big/co1wyy.png"}, 
-            {"name": "Assassin's Creed: Valhalla", "img": "https://images.igdb.com/igdb/image/upload/t_cover_big/co2ed3.png"},
-            {"name": "Mario Kart 8 Deluxe", "img": "https://images.igdb.com/igdb/image/upload/t_cover_big/co213p.png"}, 
-            {"name": "Call of Duty: Black Ops II", "img": "https://images.igdb.com/igdb/image/upload/t_cover_big/co1wkv.png"}, 
-            {"name": "Halo: Reach", "img": "https://images.igdb.com/igdb/image/upload/t_cover_big/co1xha.png"}
-        ]}
+    return parse_json(listings_collection.find_one())
 
 
 if __name__ == "__main__":
