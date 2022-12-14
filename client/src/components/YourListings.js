af52e1a5d59b34f009ad6cc6088eb44dde7927e7
Yaseen Mustapha <ymustapha@deltacontrols.com>
2022-12-14 11:37:37 -0800

    Show loading listings while listings is undefined

--- client/src/components/YourListings.js
+++ client/src/components/YourListings.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useCookies } from 'react-cookie';
 
 function YourListings() {
-  const [data, setData] = useState([])
+  const [listings, setListings] = useState()
   const [cookies,] = useCookies(["jwt", "username"]);
   const isAuthenticated = !!cookies.jwt
 
@@ -20,7 +20,7 @@ function YourListings() {
           const user_id = responseProfile.data.profile.user_id;
           const responseListings = await axios.get("/listings", { params: {user_id} });
           console.log('Listings response', responseListings)
-          setData(responseListings.data)
+          setListings(responseListings.data)
         }
       } catch (error) {
         console.log("Get user profile error: ", error);
@@ -36,10 +36,11 @@ function YourListings() {
         <Container>
           <h3>Your listings:</h3>
           <Row>
-            {!data || data.length === 0 ? (
+            {!listings && <p>Loading listings...</p>}
+            {listings && listings.length === 0 ? (
               <p>No listings available</p>
             ) : (
-              data.map((listing, i) => (
+              listings && listings.map((listing, i) => (
                 <Col key={i}>
                   <h5 className="text-center">{listing.name}</h5>
                   <img style={{ width: 200, height: 250, display: "block", margin: "auto" }} src={listing.img} alt="Game" />
