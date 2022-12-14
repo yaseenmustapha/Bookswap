e99f7c06be5cf6d444e4629157c3fc7423102f41
Yaseen Mustapha <ymustapha@deltacontrols.com>
2022-12-14 01:23:10 -0800

    Listings retrieved based on logged in user_id

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import Header from '../components/Header'
 import YourListings from '../components/YourListings'
 import SearchBar from '../components/SearchBar'
+import { useCookies } from 'react-cookie';
 
 function Home() {
+    const [cookies,] = useCookies(["jwt", "username"]);
+    const isAuthenticated = !!cookies.jwt
+
     return (
         <div>
-            <Header></Header>
-            <div class="row" style={{paddingTop: "50px", paddingBottom: "50px"}}>
-                <div class="col-md-3"></div>
-                <div class="col-md-6 col-md-offset-3">
-                    <SearchBar></SearchBar>
+            <Header />
+            <div className="row" style={{paddingTop: "50px", paddingBottom: "50px"}}>
+                <div className="col-md-3"></div>
+                <div className="col-md-6 col-md-offset-3">
+                    <SearchBar />
                 </div>
-                <div class="col-md-3"></div>
+                <div className="col-md-3"></div>
             </div>
-            <YourListings></YourListings>
+            {isAuthenticated && <YourListings />}
+            
         </div>
         
     );
