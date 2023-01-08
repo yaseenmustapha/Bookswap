6389ba92b9927a6dc34f3c8f68ae53705344a45c
Yaseen Mustapha <ymustapha@deltacontrols.com>
2023-01-08 00:52:20 -0600

    Fix folder capitalization

---
+++ client/src/Components/Header.js
@@ -0,0 +1,343 @@
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Flex,
+  Text,
+  Image,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useDisclosure,
+  Center,
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+
+export default function Header() {
+  const { isOpen, onToggle } = useDisclosure();
+  const navigate = useNavigate();
+  const [cookies, , removeCookie] = useCookies(["jwt", "username"]);
+  const isAuthenticated = !!cookies.jwt;
+
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+
+  const logout = (e) => {
+    removeCookie("jwt", { path: "/" });
+    removeCookie("username", { path: "/" });
+    navigate("/");
+  };
+
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Flex
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Link
+            href={"/"}
+            _hover={{
+              textDecoration: "none",
+              color: linkHoverColor,
+            }}
+          >
+          <Image h="10" src={require("../Images/gameswap_logo.png")} />
+          </Link>
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 1 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          {isAuthenticated ? (
+            <>
+              <Center>
+                <Link
+                  href={"#"}
+                  fontSize={"sm"}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: "none",
+                    color: linkHoverColor,
+                  }}
+                >
+                  Welcome, {cookies.username}
+                </Link>
+              </Center>
+              <Link href={"/createlisting"} _hover={{ textDecoration: "none" }}>
+                <Button
+                  display={{ base: "none", md: "inline-flex" }}
+                  fontSize={"sm"}
+                  fontWeight={600}
+                  color={"white"}
+                  bg={"green.500"}
+                  href={"/createlisting"}
+                  _hover={{
+                    bg: "green.400",
+                  }}
+                >
+                  Create Listing
+                </Button>
+              </Link>
+              <Button
+                as={"a"}
+                fontSize={"sm"}
+                fontWeight={400}
+                variant={"link"}
+                onClick={logout}
+              >
+                Log Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                as={"a"}
+                fontSize={"sm"}
+                fontWeight={400}
+                variant={"link"}
+                href={"/login"}
+              >
+                Sign In
+              </Button>
+              <Link href={"/register"} _hover={{ textDecoration: "none" }}>
+                <Button
+                  display={{ base: "none", md: "inline-flex" }}
+                  fontSize={"sm"}
+                  fontWeight={600}
+                  color={"white"}
+                  bg={"blue.400"}
+                  href={"/register"}
+                  _hover={{
+                    bg: "blue.300",
+                  }}
+                >
+                  Sign Up
+                </Button>
+              </Link>
+            </>
+          )}
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+
+  return (
+    <Stack direction={"row"} mt={2} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? "#"}
+                fontSize={"sm"}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: "none",
+                  color: linkHoverColor,
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+const DesktopSubNav = ({ label, href, subLabel }) => {
+  return (
+    <Link
+      href={href}
+      role={"group"}
+      display={"block"}
+      p={2}
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("blue.50", "gray.900") }}
+    >
+      <Stack direction={"row"} align={"center"}>
+        <Box>
+          <Text
+            transition={"all .3s ease"}
+            _groupHover={{ color: "blue.400" }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize={"sm"}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
+          opacity={0}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
+          flex={1}
+        >
+          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+const NAV_ITEMS = [
+  {
+    label: "About Us",
+    children: [
+      {
+        label: "Our Team",
+        subLabel: "We are so epic",
+        href: "#",
+      },
+      {
+        label: "Our Story",
+        subLabel: "An epic saga",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Contact Us",
+    href: "#",
+  },
+];
---
+++ client/src/Components/YourListings.js
@@ -0,0 +1,117 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Divider,
+  Heading,
+  Image,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+
+function YourListings() {
+  const [listings, setListings] = useState();
+  const [cookies] = useCookies(["jwt", "username"]);
+  const isAuthenticated = !!cookies.jwt;
+
+  useEffect(() => {
+    const getListingsForCurrentUser = async () => {
+      try {
+        const responseProfile = await axios.get("/user", {
+          headers: { Authorization: `Bearer ${cookies.jwt}` },
+        });
+        console.log(
+          "Get user profile response: ",
+          responseProfile.data.profile
+        );
+        if (responseProfile.status === 200) {
+          const user_id = responseProfile.data.profile.user_id;
+          const responseListings = await axios.get("/listings", {
+            params: { user_id },
+          });
+          console.log("Listings response", responseListings);
+          setListings(responseListings.data);
+        }
+      } catch (error) {
+        console.log("Get user profile error: ", error);
+      }
+    };
+
+    if (isAuthenticated) {
+      getListingsForCurrentUser();
+    }
+  }, []);
+
+  return (
+    <>
+      <Text fontSize="2xl" pl={"40px"}>
+        Your Listings
+      </Text>
+      {!listings && (
+        <Skeleton isLoaded={listings}>
+          <Box h={"300px"} />
+        </Skeleton>
+      )}
+
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
+        {!listings && <p>Loading listings...</p>}
+        {listings && listings.length === 0 ? (
+          <Text fontSize="xl" pl={"40px"}>
+            No listings available
+          </Text>
+        ) : (
+          listings &&
+          listings.map((listing, i) => (
+            <Card maxW="sm">
+              <CardBody>
+                <Center>
+                  {listing.img && (
+                    <Image
+                      src={listing.img[0]}
+                      alt={listing.name}
+                      borderRadius="lg"
+                    />
+                  )}
+                </Center>
+
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">{listing.name}</Heading>
+                  <Text>{listing.description}</Text>
+                  <Text color="blue.600" fontSize="2xl">
+                    ${Number(listing.price).toFixed(2)}
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Edit
+                  </Button>
+                  <Button variant="ghost" colorScheme="red">
+                    Delete
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          ))
+        )}
+      </SimpleGrid>
+    </>
+  );
+}
+
+export default YourListings;
